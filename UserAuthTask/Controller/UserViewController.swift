6ab28f6efbc2b66767b6859ae4511820aedee4a1
Lukasz <dziwosz@icloud.com>
2021-08-22 19:59:47 +0200

    user view outlets connected

--- UserAuthTask/Controller/UserViewController.swift
+++ UserAuthTask/Controller/UserViewController.swift
@@ -9,5 +9,19 @@ import UIKit
 
 class UserViewController: UIViewController {
     
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var fullNameLbl: UILabel!
+    @IBOutlet weak var addressLbl: UILabel!
+    @IBOutlet weak var telephoneLbl: UILabel!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
+    
+    @IBAction func logoutBtnAction(_ sender: Any) {
+    }
 }
