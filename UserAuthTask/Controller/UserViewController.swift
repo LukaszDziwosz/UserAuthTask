4656e6c8efeb2787f1ebdc44eb69443eeac53802
Lukasz <dziwosz@icloud.com>
2021-08-22 21:52:39 +0200

    receiving user data

--- UserAuthTask/Controller/UserViewController.swift
+++ UserAuthTask/Controller/UserViewController.swift
@@ -7,21 +7,59 @@
 
 import UIKit
 
-class UserViewController: UIViewController {
+class UserViewController: UIViewController , UserDataDelegate {
     
     @IBOutlet weak var userImageView: UIImageView!
     @IBOutlet weak var fullNameLbl: UILabel!
     @IBOutlet weak var addressLbl: UILabel!
     @IBOutlet weak var telephoneLbl: UILabel!
     
+    let networking = Networking()
+    let alertService = AlertService()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        networking.delegate = self
     }
     override func viewWillAppear(_ animated: Bool) {
-        <#code#>
+        super.viewWillAppear(animated)
+        getUserInfo()
+        
     }
     
     @IBAction func logoutBtnAction(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.removeObject(forKey: "jsonwebtoken")
+        defaults.removeObject(forKey: "refreshtoken")
     }
+    
+    func getUserInfo() {
+        
+        let defaults = UserDefaults.standard
+        guard let token = defaults.string(forKey: "jsonwebtoken") else {
+            return
+        }
+        networking.requestUser(endpoint: "/user", token: token) { [weak self] (result) in
+            switch result {
+
+            case .success(let userData): print("\(userData)")
+            case.failure(let error):
+                print(error.localizedDescription)
+                guard let alert = self?.alertService.alert(message: "Cannot get user data") else { return }
+                self?.present(alert, animated: true)
+            }
+        }
+    }
+    func didGetData(_ userData: User) {
+        DispatchQueue.main.async {
+            self.fullNameLbl.text = "\(userData.firstName) \(userData.lastName)"
+            self.addressLbl.text = userData.address
+            self.telephoneLbl.text = userData.phone
+            
+            
+        }
+        
+    }
+    
 }
--- UserAuthTask/Model/Networking.swift
+++ UserAuthTask/Model/Networking.swift
@@ -4,11 +4,15 @@
 //
 //  Created by Lukasz Dziwosz on 22/08/2021.
 //
-
+protocol UserDataDelegate {
+    func didGetData (_ userData: User)
+}
 import Foundation
 
 
 class Networking {
+    
+    var delegate: UserDataDelegate?
     
     let baseUrl = "https://vidqjclbhmef.herokuapp.com"
     
@@ -98,6 +102,70 @@ class Networking {
         
         task.resume()
     }
+    func requestUser(endpoint: String, token: String, completion: @escaping (Result<User, Error>) -> Void) {
+        guard let url = URL(string: baseUrl + endpoint) else {
+            completion(.failure(NetworkingError.badUrl))
+            return
+        }
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        handleUserResponse(for: request, completion: completion)
+    }
+    func handleUserResponse(for request: URLRequest,
+                        completion: @escaping (Result<User, Error>) -> Void) {
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            DispatchQueue.main.async {
+                
+                guard let unwrappedResponse = response as? HTTPURLResponse else {
+                    completion(.failure(NetworkingError.badResponse))
+                    return
+                }
+                 print(unwrappedResponse.statusCode)
+                switch unwrappedResponse.statusCode {
+                    
+                case 200 ..< 300:
+                    print("success")
+                    
+                default:
+                    print("failure")// send refresh token here
+                }
+               
+                if let unwrappedError = error {
+                    completion(.failure(unwrappedError))
+                  
+                    return
+                }
+                
+                if let unwrappedData = data {
+                    
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: unwrappedData, options: [])
+                        print(json)
+                        
+                        if let userData = try? JSONDecoder().decode(User.self, from: unwrappedData) {
+                            completion(.success(userData))
+                            self.delegate?.didGetData(userData)
+                        } else {
+                            let errorResponse = try JSONDecoder().decode(ErrorResponse.self, from: unwrappedData)
+                            completion(.failure(errorResponse))
+                            
+                        }
+                        
+                    } catch {
+                        completion(.failure(error))
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+    }
+
 }
 enum NetworkingError: Error {
     case badUrl
